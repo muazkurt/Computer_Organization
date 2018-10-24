d479a735136ddcaf7e207c93f32bb8bf2de36f4e
Muaz Kurt <muazkurt@gmail.com>
2018-10-24 11:32:48 +0300

    ++++File read. -----Check&Update.

--- HW 01/Homework 1.asm
+++ HW 01/Homework 1.asm
@@ -1,62 +1,71 @@
 		.data 
 welcome:	.asciiz "Welcome\nPlease give filename: "
-input:		.asciiz "testout.txt"
-buffer:		.word '\0'
 
 
 		.text
 main:
 	jal	Open_file
-	add	$s1, $zero, $v0
-#Read file
-	la	$s2, buffer
+	add	$s0, $zero, $v0
 	
-	
-	
-read:	addi	$v0, $zero, 14
-	add	$a0, $zero, $s1
-	add	$a1, $zero, $s2
-	addi	$a2, $zero, 1
+	addi	$sp $sp -256
+	add	$s1 $sp $zero
+	#read file
+	addi	$v0, $zero, 14
+	add	$a0, $zero, $s0
+	add	$a1, $zero, $s1
+	addi	$a2, $zero, 256
 	syscall
-	beq	$v0, $zero, loopexit
-	sb	$zero 1($s2)
-	addi	$v0 $zero 4
-	add	$a0 $zero $s2
-	syscall
-	addi	$s2, $s2, 1
-	j	read
-loopexit:
-	add	$a0, $zero, $s1
+	#close file
+	add	$a0, $zero, $s0
 	addi	$v0, $zero, 16
 	syscall
 	
-	addi	$sp, $sp, +100
+	#parameters
+	jal check
+	
 	addi	$v0, $zero, 10
 	syscall
+#end of main
+
+
+
+check:
+	beq	$v0, $zero, loopexit
+	
+	addi	$v0 $zero 11
+	lb	$a0 0($a1)
+	syscall
+	addi	$v0 $zero 14
+	j	check
+loopexit:
+
+	addi	$sp $sp 256
+
+	jr	$ra
+#end of check	
 
 
 Open_file:
-#Write welcome
+	#Write welcome
 	addi	$v0 $zero 4
 	la	$a0 welcome
 	syscall
-#Read filename
+	#Read filename
 	addi	$sp $sp -256
 	add	$s0 $sp $zero
-	add	$a0 $s0 $zero
+	add	$t0 $s0 $zero
 	addi	$t1 $zero 10
-	addi	$a1 $zero 2
 
 	read_filename:
-		addi	$v0 $zero 8
+		addi	$v0 $zero 12
 		syscall
-		lb	$t2 0($a0)
-		beq	$t1 $t2 end_read_filename
-		addi	$a0 $a0 1
+		beq	$t1 $v0 end_read_filename
+		sb	$v0 0($t0)
+		addi	$t0 $t0 1
 		j	read_filename
 	end_read_filename:
-	sb	$zero 0($a0)
-#Open file
+	sb	$zero 0($t0)
+	#Open file
 	addi	$v0, $zero, 13
 	add	$a0 $s0 $zero
 	addi	$a1, $zero, 0
@@ -64,3 +73,4 @@ Open_file:
 	syscall
 	addi	$sp $sp 256
 	jr	$ra
+#end of Open_file
