20b9925a3c3ec898ce3c3d839cc2864273d296d2
Muaz Kurt <muazkurt@gmail.com>
2018-10-24 18:51:19 +0300

    +++++Number->String. --------Exceptions.

--- HW 01/Homework 1.asm
+++ HW 01/Homework 1.asm
@@ -1,6 +1,25 @@
 		.data 
 welcome:	.asciiz "Welcome\nPlease give filename: "
 
+zero:	.asciiz "zero"
+
+one:	.asciiz "one"
+
+two:	.asciiz "two"
+
+three:	.asciiz "three"
+
+four:	.asciiz "four"
+
+five:	.asciiz "five"
+
+six:	.asciiz "six"
+
+seven:	.asciiz "seven"
+
+eight:	.asciiz "eight"
+
+nine:	.asciiz "nine"
 
 		.text
 main:
@@ -15,7 +34,7 @@ main:
 	add	$a1, $zero, $s1
 	addi	$a2, $zero, 256
 	syscall
-	addi	$a1 $a1 $v0
+	add	$a1 $a1 $v0
 	sb	$zero 0($a1)
 	#close file
 	add	$a0, $zero, $s0
@@ -23,7 +42,8 @@ main:
 	syscall
 	
 	#parameters
-	jal check
+	add	$a0 $zero $s1
+	jal	check
 	
 	addi	$v0, $zero, 10
 	syscall
@@ -31,30 +51,78 @@ main:
 
 
 
+
+	#a0 -> address of the input array. 
 check:
-	while(*a != 0)
-		if(*a >= 0 &&  *a <= '9')
-			write content
-		else
-			write content
-		++a.
-		
-	sw	$s0 0($sp)
-	add	$s0 $a0 $zero
-	add	$t0 $a0 $zero
-sub_routine:	
-	lb	$t1 0($t0)
-	beq	$t1, $zero, loopexit
 	
-	addi	$v0 $zero 11
-	lb	$a0 0($a1)
-	syscall
-	addi	$v0 $zero 14
-	j	check
+	
+
+
+	addi	$sp $sp -4
+	sw	$s0 0($sp)
+	add	$s0 $zero $a0
+	add	$t0 $zero $s0
+	addi	$t1 $zero 47		# prewious of '0' 
+	addi	$t2 $zero 58		# next item of '9'
+sub_routine:	
+	lb	$t3 0($t0)
+	beq	$t3, $zero, loopexit
+
+	#if (*a <= '9' && *a >= '0')
+		slt	$t4 $t1 $t3
+		slt	$t5 $t3 $t2
+		beq	$t4 $t5 true
+		#false
+			addi	$v0 $zero 11
+			add	$a0 $zero $t3
+			syscall
+			j both
+		
+		#end false
+
+		true:	
+			addi	$t6 $t1 1
+			la	$a0 zero
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 one
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 two
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 three
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 four
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 five
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 six
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 seven
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 eight
+			beq	$t3 $t6 after
+			addi	$t6 $t6 1
+			la	$a0 nine
+			beq	$t6 $t6 after
+			j both
+			after:
+			addi	$v0 $zero 4
+			syscall
+		#end else
+both:
+	addi	$t0 $t0 1
+	j	sub_routine
+	
+	
 loopexit:
-
 	addi	$sp $sp 256
-
 	jr	$ra
 #end of check	
 
@@ -88,3 +156,8 @@ Open_file:
 	addi	$sp $sp 256
 	jr	$ra
 #end of Open_file
+
+
+
+
+
